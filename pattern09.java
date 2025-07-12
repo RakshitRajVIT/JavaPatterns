e7ededcdd1d09c02aa431e338e9a62730ee6bc82
RakshitRajVIT <rakshitraj2323@gmail.com>
2025-07-12 16:25:59 +0530

    initial Commit

--- pattern09.java
+++ pattern09.java
@@ -1,17 +1,35 @@
 package JavaPatterns;
 
+// Pattern 09: X Pattern
+// This program prints an 'X' shape using asterisks '*'
+// Example output for n = 5:
+// *   *
+//  * *
+//   *
+//  * *
+// *   *
+
 public class pattern09 {
 
     public static void main(String[] args) {
-        int n = 5;
+        int n = 5; // Size of the pattern (number of rows and columns)
+
+        // Outer loop for each row
         for (int i = 1; i <= n; i++) {
+
+            // Inner loop for each column in the current row
             for (int j = 1; j <= n; j++) {
+
+                // Print '*' at diagonal positions
                 if (j == i || j == n - i + 1) {
                     System.out.print("*");
                 } else {
+                    // Print space elsewhere
                     System.out.print(" ");
                 }
             }
+
+            // Move to the next line after printing each row
             System.out.println();
         }
     }
